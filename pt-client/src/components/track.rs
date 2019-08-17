c0ab8d4fab112dbbcc93fab863b4df89d4442fe1
James Kolsby <jrkolsby@mac.com>
2019-08-17 17:46:36 -0400

    feat: l

--- pt-client/src/components/track.rs
+++ pt-client/src/components/track.rs
@@ -1,7 +1,21 @@
+mod waveform;
+
 fn render(
         muted: bool,
         armed: bool,
         soloed: bool
     ) -> String {
     return "Hi".to_string();
+}
+
+struct Track {
+    muted: bool,
+    armed: bool,
+    solo: bool,
+}
+
+impl Track {
+    pub fn new() -> Self {
+        let 
+    }
 }
--- pt-client/src/components/waveform.rs
+++ pt-client/src/components/waveform.rs
@@ -1,9 +1,58 @@
 use wavefile::WaveFile;
 use itertools::Itertools;
 
+use cursive::theme::{BaseColor, Color, ColorStyle};
+
 const WAVEFORM_WIDTH: usize = 20;
 const WAVEFORM_HEIGHT: usize = 4;
 
+struct Waveform {
+    sound_file: &str,
+    color: Color,
+    Vec<(i32, i32)> pairs,
+}
+
+impl cursive::view::View for Waveform {
+    fn draw(&self, printer: &printer) {
+        for (i, pair) in self.pairs.iter().enumerate() {
+            text = pair_to_char(pair);
+            printer.with_color(
+                ColorStyle::new(Color::Dark(BaseColor::White), color),
+                |printer| printer.print((i, 0), text),
+            )
+        }
+    }
+
+    fn take_focus(&mut self, _: Direction) -> bool {
+        true
+    }
+
+    fn on_event(&mut self, event: Event) -> EventResult {
+        EventResult::Ignored
+    }
+
+    fn required_size(&mut self, _: Vec2) -> Vec2 {
+        Vec2(self.pairs.size, 1)
+    }
+    
+    fn pair_to_char(pair: (i32, i32)) -> char {
+
+    // MAX IS 65,536
+    let a: usize = (pair.0 >= 1) as usize;
+    let b: usize = (pair.0 >= 2) as usize;
+    let c: usize = (pair.0 >= 3) as usize;
+    let d: usize = (pair.0 >= 4) as usize;
+    let e: usize = (pair.1 >= 1) as usize;
+    let f: usize = (pair.1 >= 2) as usize;
+    let g: usize = (pair.1 >= 3) as usize;
+    let h: usize = (pair.1 >= 4) as usize;
+
+    braille::BRAILLE[d][h]
+                    [c][g]
+                    [b][f]
+                    [a][e]
+}
+}
 // Cursive Functions
 // pub fn step(&mut self) -> bool
 // index.add_global_callback('l', |s| s.quit());
@@ -31,7 +80,7 @@ pub fn render(input: &str) -> String {
     //println!("GLOBAL_MAX: {}", global_max);
     //println!("SCALE: {}", scale);
 
-    let mut base: String = "".to_string();
+    let mut base: String = "".to_string(;
     for (i, value) in values.iter().enumerate() {
         if i % 2 > 0 {
             continue;
@@ -49,22 +98,4 @@ pub fn render(input: &str) -> String {
         base = format!("{}{}", base, unit);
     }
     base
-}
-
-pub fn pair_to_char(pair: (i32, i32)) -> char {
-
-    // MAX IS 65,536
-    let a: usize = (pair.0 >= 1) as usize;
-    let b: usize = (pair.0 >= 2) as usize;
-    let c: usize = (pair.0 >= 3) as usize;
-    let d: usize = (pair.0 >= 4) as usize;
-    let e: usize = (pair.1 >= 1) as usize;
-    let f: usize = (pair.1 >= 2) as usize;
-    let g: usize = (pair.1 >= 3) as usize;
-    let h: usize = (pair.1 >= 4) as usize;
-
-    braille::BRAILLE[d][h]
-                    [c][g]
-                    [b][f]
-                    [a][e]
 }
