cbb2c1bc9af372dc138bc8bd63ecbe9d1b2e0c7d
James Kolsby <jrkolsby@mac.com>
2020-04-27 01:15:40 -0400

    feat: simplified svg output

--- pt-design/beta/src/render_font.py
+++ pt-design/beta/src/render_font.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFont, ImageDraw
 import cv2
 import numpy as np
+from shapely import geometry, ops
 
 # FONT RENDERING
 
@@ -14,36 +15,15 @@ font = ImageFont.truetype("fonts/OsakaMono.ttf", SIZE) # Load font
 char_img = Image.new("L", (SIZE, SIZE), 0) #Initialize black bg
 draw = ImageDraw.Draw(char_img)
 # 人攴/~彡厂二工田冂冖丿艸匚爪巛州儿冫彳
-draw.text((0, 0), "~", font=font, fill=(255)) # Draw white char
+draw.text((0, 0), ")", font=font, fill=(255)) # Draw white char
 
 img = np.array(char_img) # Convert to numpy array
 cv2.imwrite("bitmap.png", img)
 
-'''
-size = np.size(img)
-skel = np.zeros(img.shape,np.uint8)
-
-element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-done = False
-
-while(not done):
-    eroded = cv2.erode(img,element)
-    temp = cv2.dilate(eroded,element)
-    temp = cv2.subtract(img,temp)
-    skel = cv2.bitwise_or(skel,temp)
-    img = eroded.copy()
-
-    zeros = size - cv2.countNonZero(img)
-    if zeros == size:
-        done = True
-
-cv2.imwrite("skeleton.png", skel)
-'''
-
 # SKELETONIZATION and PATH DETECTION (perform hough transform)
 
-kernel = np.ones((5, 5), np.uint8) 
-output = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+kernel = np.ones((9, 9), np.uint8) 
+# output = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 lines = []
 done = False
 
@@ -59,19 +39,38 @@ while not done:
     if len(lines) <= MAX_LINES:
         done = True
 
+# SIMPLIFY LINE TOPOLOGY WITH MERGE
+
+line_string = [] 
+for line in lines:
+    for x1, y1, x2, y2 in line:
+        line_string.append(geometry.LineString([[x1,y1], [x2,y2]]))
+
+multi_line = geometry.MultiLineString(line_string)
+
+print(multi_line)
+
+merged_line = ops.linemerge(multi_line)
+
+print(merged_line)  # prints LINESTRING (0 0, 1 1, 2 2, 3 3)
+
 # WRITE TO SVG (TODO: DATABASE)
 
 f = open('path.svg', 'w+')
-f.write('<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">')
+f.write('<svg width="{0}" height="{1}" xmlns="http://www.w3.org/2000/svg">'.format(SIZE, SIZE))
 
-f.write('<path d="M')
-for line in lines:
-    for x1,y1,x2,y2 in line:
-        f.write('{0} {1} {2} {3}'.format(x1, y1, x2, y2))
-        cv2.line(output,(x1,y1),(x2,y2),(0,255,0),1)
-f.write('"/>')
 
-f.write('</svg>')
+for line in merged_line:
+    f.write('<path stroke="red" stroke-width="1" d="M')
+    print('\n')
+    for x, y in line.coords:
+        print(x, y)
+        f.write('{0} {1} '.format(int(x), int(y)))
+        # cv2.line(output,(int(x1),int(y1)),(int(x2),int(y2)),(0,255,0),1)
+    f.write('"/>')
+
+
+f.write('"</svg>')
 f.close()
 
-cv2.imwrite('output.png', output)
+# cv2.imwrite('output.png', output)
